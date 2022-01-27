5e5ae75c49e84aa690c71a7887bdeafd3b80d21e
Kornull <wolod1987kor@gmail.com>
2022-01-27 16:08:59 +0300

    fix: add ';'

--- src/index.js
+++ src/index.js
@@ -45,29 +45,29 @@ function decode(expr) {
 
   // collect an array
   for (let i = 0; i < exp.length; i += 10) {
-    codeLists.push(exp.slice(i, i + 10))
+    codeLists.push(exp.slice(i, i + 10));
   }
 
   // Concatenate each element of the array
-  const codeList = codeLists.map((x) => x.join(''))
+  const codeList = codeLists.map((x) => x.join(''));
   // Delete stars
-  const noStars = codeList.map((el) => (el === '**********') ? ' ' : el)
+  const noStars = codeList.map((el) => (el === '**********') ? ' ' : el);
   // Delete zero
-  const zeroDelete = codeList.map((x) => Number(x).toString())
+  const zeroDelete = codeList.map((x) => Number(x).toString());
 
   for (let code of zeroDelete) {
-    morse = ''
+    morse = '';
     // Collect morse
     for (let i = 1; i < code.length; i += 2) {
-      if (code[i] !== '1' && code[i] !== '0') { morse += '*'; }
-      if (code[i] === '1') { morse += '-'; }
-      if (code[i] === '0') { morse += '.'; }
+      if (code[i] !== '1' && code[i] !== '0') { morse += '*'; };
+      if (code[i] === '1') { morse += '-'; };
+      if (code[i] === '0') { morse += '.'; };
     }
     // Delete 'undefined'
     if (morse === '*') { decoder += ' ' } else {
-      decoder += MORSE_TABLE[morse]
+      decoder += MORSE_TABLE[morse];
     }
-  } return decoder
+  } return decoder;
 }
 
 
